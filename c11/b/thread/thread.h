477f7a5d864c27a7a0461b99bb0b3100242bee06
黑牛 <chenlingmin.com@gmail.com>
2020-11-05 09:57:13 +0800

    进程实现

--- c11/b/thread/thread.h
+++ c11/b/thread/thread.h
@@ -2,6 +2,7 @@
 #define __THREAD_THREAD_H
 #include "stdint.h"
 #include "list.h"
+#include "memory.h"
 
 /* 自定义通用函数类型,它将在很多线程函数中做为形参类型 */
 typedef void thread_func(void*);
@@ -91,8 +92,13 @@ struct task_struct {
 
     uint32_t* pgdir;    // 进程自己页表的虚拟地址
 
+    struct virtual_addr userprog_vaddr; // 用户进程的虚拟地址
+
     uint32_t stack_magic;   // 用这串数字做栈的边界标记,用于检测栈的溢出
 };
+
+extern struct list thread_ready_list;
+extern struct list thread_all_list;
 
 void thread_create(struct task_struct* pthread, thread_func function, void* func_arg);
 void init_thread(struct task_struct* pthread, char* name, int prio);
