cad8f5016b479cc2382c389fbf10131cb54a15ab
黑牛 <chenlingmin.com@gmail.com>
2020-11-26 11:10:20 +0800

    修正 tss 错误

--- c14/h/userprog/tss.c
+++ c14/h/userprog/tss.c
@@ -47,7 +47,7 @@ static struct gdt_desc make_gdt_desc(uint32_t* desc_addr, uint32_t limit, uint8_
     struct gdt_desc desc;
     desc.limit_low_word = limit & 0x0000ffff;
     desc.base_low_word  = desc_base & 0x0000ffff;
-    desc.base_mid_byte  = ((desc_base && 0x00ff0000) >> 16);
+    desc.base_mid_byte  = ((desc_base & 0x00ff0000) >> 16);
     desc.attr_low_byte  = (uint8_t)(attr_low);
     desc.limit_high_attr_high = (((limit & 0x000f0000) >> 16) + (uint8_t)(attr_high));
     desc.base_high_byte = desc_base >> 24;
