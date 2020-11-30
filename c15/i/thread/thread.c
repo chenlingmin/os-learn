29b6c943e706cfbc454e50d4b8db09ccfe50a3e1
黑牛 <chenlingmin.com@gmail.com>
2020-11-30 14:10:22 +0800

    修正 pad_print

--- c15/i/thread/thread.c
+++ c15/i/thread/thread.c
@@ -250,8 +250,10 @@ static void pad_print(char* buf, int32_t buf_len, void* ptr, char format) {
             break;
         case 'd':
             out_pad_0idx = sprintf(buf, "%d", *((int16_t*)ptr));
+            break;
         case 'x':
             out_pad_0idx = sprintf(buf, "%x", *((uint32_t*)ptr));
+            break;
     }
     while (out_pad_0idx < buf_len) {    // 以空格填充
         buf[out_pad_0idx] = ' ';
