d61779d520679269284497ef1cda678f36a93fa5
黑牛 <chenlingmin.com@gmail.com>
2020-11-30 13:56:38 +0800

    去除调试信息

--- c15/i/userprog/fork.c
+++ c15/i/userprog/fork.c
@@ -29,9 +29,6 @@ static int32_t copy_pcb_vaddrbitmap_stack0(struct task_struct* child_thread, str
      * 下面将child_thread->userprog_vaddr.vaddr_bitmap.bits指向自己的位图vaddr_btmp */
     memcpy(vaddr_btmp, child_thread->userprog_vaddr.vaddr_bitmap.bits, bitmap_pg_cnt * PG_SIZE);
     child_thread->userprog_vaddr.vaddr_bitmap.bits = vaddr_btmp;
-    /* 调试用 */
-    ASSERT(strlen(child_thread->name) < 11);    // pcb.name的长度是16,为避免下面strcat越界
-    strcat(child_thread->name, "_fork");
     return 0;
 }
 
