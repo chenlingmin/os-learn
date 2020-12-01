b51b262f160ef584a75b4084c75c782b41755b83
黑牛 <chenlingmin.com@gmail.com>
2020-12-01 15:49:16 +0800

    命令管道

--- c15/k/kernel/main.c
+++ c15/k/kernel/main.c
@@ -20,12 +20,12 @@ void main(void) {
     init_all();
 
 /*************    写入应用程序    *************/
-//    uint32_t file_size = 5108;
+//    uint32_t file_size = 5440;
 //    uint32_t sec_cnt = DIV_ROUND_UP(file_size, 512);
 //    struct disk* sda = &channels[0].devices[0];
 //    void* prog_buf = sys_malloc(file_size);
 //    ide_read(sda, 300, prog_buf, sec_cnt);
-//    int32_t fd = sys_open("/prog_pipe", O_CREAT|O_RDWR);
+//    int32_t fd = sys_open("/cat", O_CREAT|O_RDWR);
 //    if (fd != -1) {
 //        if(sys_write(fd, prog_buf, file_size) == -1) {
 // 	        printk("file write error!\n");
